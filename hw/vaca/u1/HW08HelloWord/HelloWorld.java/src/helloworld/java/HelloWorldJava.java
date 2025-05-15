2d915af0965e05b928de6f19a44def70ac306f77
Kevin Vaca <kevin.vaca.05@hotmail.com>
2025-05-14 21:47:22 -0500

    HW Update

---
+++ hw/vaca/u1/HW08HelloWord/HelloWorld.java/src/helloworld/java/HelloWorldJava.java
@@ -0,0 +1,11 @@
+package helloworld.java;
+
+/**
+ *
+ * @author Kevin Vaca Edison's OOP ESPE
+ */
+public class HelloWorldJava {
+    public static void main(String[] args) {
+            System.out.println("Hello World.");
+    }
+}
