9dd8371a2145624711b0b0cebda6c62aea743b98
GeraldAstudilloOrtega <giastudillo@espe.edu.ec>
2025-05-26 20:53:57 -0500

    Upload ws

---
+++ ws/astudillo/U1/WS12ProjectStructure/ChickenFarmSimulator/src/ec/edu/espe/chickenfarmsimulator/model/Egg.java
@@ -0,0 +1,19 @@
+package ec.edu.espe.chickenfarmsimulator.model;
+
+/**
+ *
+ * @author Gerald Astudillo SoftCrafters DCCO ESPE
+ */
+
+public class Egg {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+}
