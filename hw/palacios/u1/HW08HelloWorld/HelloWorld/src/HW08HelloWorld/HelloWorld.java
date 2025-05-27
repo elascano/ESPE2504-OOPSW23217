0cb3f835c4cabf81b99ef12559307db8fcf92a63
Daniel Palacios <depalacios2@espe.edu.ec>
2025-05-26 23:41:31 -0500

    HW08HelloWorld

---
+++ hw/palacios/u1/HW08HelloWorld/HelloWorld/src/HW08HelloWorld/HelloWorld.java
@@ -0,0 +1,13 @@
+package HW08HelloWorld;
+
+/**
+ *
+ * @author Daniel Palacios
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello programers I am Daniel Palacios");
+    }
+}
