1a748994f649b466e4cb7d5d4e0612d2ccc9a187
Pedro Isaac Maisincho Paucar <imasincho@gmail.com>
2025-06-26 19:34:12 -0500

    ExcersivetypeExam

---
+++ ws/maisincho/u2/WS23OOP/src/main/java/ec/edu/espe/view/Main.java
@@ -0,0 +1,17 @@
+
+package ec.edu.espe.view;
+
+/**
+ *
+ * @author LABS-ESPE
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
