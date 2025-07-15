b0ed26689608ad8c95d01a0ff8ca6a76f52d50fd
julitobby <julitobbyandrade@gmail.com>
2025-07-15 13:02:13 -0500

    sadaa

---
+++ ws/andrade/u3/Ws28/Taxes/src/main/java/edu/ec/espe/taxes/controller/Operation.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.ec.espe.taxes.controller;
+
+/**
+ *
+ * @author LABS-ESPE
+ */
+public class Operation {
+    public static float add(float addend1, float addend2){
+        float sum;
+        sum=addend1*addend2;
+        return sum;
+    }
+}
---
+++ ws/andrade/u3/Ws28/Taxes/src/main/java/edu/ec/espe/taxes/taxes/Taxes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.ec.espe.taxes.taxes;
+
+/**
+ *
+ * @author LABS-ESPE
+ */
+public class Taxes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
