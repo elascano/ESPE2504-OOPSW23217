ce625419634cb70facaff1062443c16c81e789e7
Pedro Isaac Maisincho Paucar <imasincho@gmail.com>
2025-07-16 00:45:12 -0500

    UnitTest

---
+++ hw/maisincho/u3/HW15UnitTest/UnitTest/src/main.py
@@ -0,0 +1,8 @@
+def sum(x, y):
+    return x + y
+
+
+def is_greater_than(number_1, number_2):
+    return number_1 > number_2
+
+
---
+++ hw/maisincho/u3/HW15UnitTest/UnitTest/src/tests/test_main.py
@@ -0,0 +1,25 @@
+import pytest
+from src.main import sum, is_greater_than
+
+
+def test_sum():
+    assert sum(2, 4) ==  6
+
+
+def test_is_greater_than():
+    assert is_greater_than(10, 2)
+
+
+@pytest.mark.parametrize(
+    "input_x, input_y, expected",
+    [
+        (5, 1, 6),
+        (6, sum(4, 2), 12),
+        (sum(19, 1), 15, 35),
+        (-7, 10, sum(-7, 10))
+    ]
+)
+def test_sum_params(input_x, input_y, expected):
+    assert sum(input_x, input_y) == expected
+
+
