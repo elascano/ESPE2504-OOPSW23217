b42a31cfe3ebd7c288af775e11b4049fd31f92cd
MATEO JOSUE UNDA MACIAS <mateounda2019@gmail.com>
2025-07-17 14:55:46 -0500

    WS

---
+++ ws/unda/U3/WS29OOPWithJS/routes/customerRouter.js
@@ -0,0 +1,28 @@
+const express = require('express'); //FRAMEWORK
+const customer = require('../models/customer'); // Importing the Customer model
+const router = express.Router(); // Creating a new router instance
+
+// GET all customers
+router.get("/customers", async (req, res) => {
+try {
+const customers = await customer.find(); // Fetching all customers from the database
+res.json(customers);
+} catch (error) {
+res.status(500).json({ message: error.message }); // Handling errors
+}
+});
+
+module.exports = router; // Exporting the router to be used in other files
+
+router.get("/customers/:id", async (req, res) => {
+try {
+const customerObject = await customer.findOne({id: req.params.id}); // Fetching a customer by ID
+if (customerObject ==null) {
+res.status(404).json(404); // Handling case where customer is not found
+} else {
+res.json(customerObject); // Returning the found customer
+}
+} catch (error) {
+res.status(500).json({ message: error.message }); // Handling errors
+}
+});
