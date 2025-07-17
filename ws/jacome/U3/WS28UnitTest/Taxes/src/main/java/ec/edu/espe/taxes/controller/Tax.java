65bedff303dcf733bc2a0038eb0755fbeda92f4b
Micaela Victoria Jacome Calahorrano <mvjacome4@espe.edu.ec>
2025-07-16 22:49:54 -0500

    WS28

---
+++ ws/jacome/U3/WS28UnitTest/Taxes/src/main/java/ec/edu/espe/taxes/controller/Tax.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.taxes.controller;
+
+/**
+ *
+ * @author LABS-ESPE
+ */
+public class Tax {
+    public float computeTax(float basePrice, float percentage, boolean charged){
+        float ivaValue = 0.0F;
+        if(!charged){
+            return 0.0F;
+        } else {
+            ivaValue = basePrice * percentage /100;
+        }
+        return ivaValue;
+    }
+    
+    public float computeIce (float basePrice){
+        //Cade thye computation of ICE
+        return 0.0F;
+    }
+    
+}
