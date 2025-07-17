2421968afdba2f2f4df272a6a97b313adc0a3165
neolukk <davidbonilla188@gmail.com>
2025-07-17 14:49:48 -0500

    WS29 update

--- ws/bonilla/U3/WS29OPPWithJs/HelloServer.js
+++ ws/bonilla/U3/WS29OPPWithJs/HelloServer.js
@@ -1,12 +1,12 @@
-const http = require('http');
+const http = require('node:http');
 
 const hostname = '127.0.0.1';
 const port = 3001;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<b>Hello</b>, <i>DAVID BONILLA</i>!\n');
 })
 
 server.listen(port, hostname, () => {
