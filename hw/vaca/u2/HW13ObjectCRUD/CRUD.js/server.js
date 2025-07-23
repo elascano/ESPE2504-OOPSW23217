c5a5cc247211e4d591437b43addb74d25421bc4a
Kevin Vaca <kevin.vaca.05@hotmail.com>
2025-07-23 08:54:20 -0500

    HWjs

--- hw/vaca/u2/HW13ObjectCRUD/CRUD.js/server.js
+++ hw/vaca/u2/HW13ObjectCRUD/CRUD.js/server.js
@@ -18,6 +18,8 @@ mongoose.connect(uri, {
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('MongoDB connection error:', err));
 
+
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
