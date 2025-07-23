3762ba7e44ff98737e59c70cf6fd6d5f7647c540
Kevin Vaca <kavaca6@espe.edu.ec>
2025-07-23 13:03:37 -0500

    WS

---
+++ ws/vaca/u3/WS30SolidP/MazeGenerator/src/main/java/ec/edu/espe/mazegenerator/mazegenerator/model/Maze.java
@@ -0,0 +1,10 @@
+package ec.edu.espe.mazegenerator.mazegenerator.model;
+
+/**
+ *
+ * @author LABS-ESPE
+ */
+public class Maze {
+    int width;
+    int height;
+}
---
+++ ws/vaca/u3/WS30SolidP/MazeGenerator/src/main/java/ec/edu/espe/mazegenerator/mazegenerator/model/Room.java
@@ -0,0 +1,11 @@
+package ec.edu.espe.mazegenerator.mazegenerator.model;
+
+/**
+ *
+ * @author LABS-ESPE
+ */
+public class Room {
+    Door doors [];
+    Room adJoiningRoom[];
+    
+}
