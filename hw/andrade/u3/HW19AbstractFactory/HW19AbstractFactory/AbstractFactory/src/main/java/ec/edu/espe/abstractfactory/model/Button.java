90a18fb4cf136405fb7e8a2b8e62280fbb4faecd
julitobby <julitobbyandrade@gmail.com>
2025-07-30 11:34:28 -0500

    help me

---
+++ hw/andrade/u3/HW19AbstractFactory/HW19AbstractFactory/AbstractFactory/src/main/java/ec/edu/espe/abstractfactory/model/Button.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package ec.edu.espe.abstractfactory.model;
+
+/**
+ *
+ * @author Micaela Jácome
+ */
+public abstract class Button {
+public String caption;
+
+    public abstract void paint();
+}
---
+++ hw/andrade/u3/HW19AbstractFactory/HW19AbstractFactory/AbstractFactory/src/main/java/ec/edu/espe/abstractfactory/model/LinuxMenu.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.abstractfactory.model;
+
+/**
+ *
+ * @author Micaela Jácome
+ */
+public class LinuxMenu extends Menu{
+
+    @Override
+    public void paint() {
+        System.out.println("I'm a WinMenu: " + caption);
+    }
+}
