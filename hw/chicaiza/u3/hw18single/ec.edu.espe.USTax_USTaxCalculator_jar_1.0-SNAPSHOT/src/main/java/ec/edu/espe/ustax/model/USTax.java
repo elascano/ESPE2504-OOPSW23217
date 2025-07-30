a3a86b0836e182a8f514123c58b8b46da52be484
Micaela <michaelch3002@gmail.com>
2025-07-30 12:26:54 -0500

    ws

---
+++ hw/chicaiza/u3/hw18single/ec.edu.espe.USTax_USTaxCalculator_jar_1.0-SNAPSHOT/src/main/java/ec/edu/espe/ustax/model/USTax.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.ustax.model;
+
+/**
+ *
+ * @author Chicaiza Michael
+ */
+public class USTax {
+    private static USTax instance;
+    private USTax(USTax instance){
+        this.instance = instance;
+        
+    }
+    public static USTax getInstance(){
+        if(instance==null)
+            instance = new USTax();
+        return instance;
+    }
+    
+    public float CalculatesalesTotal(){
+        return 0.0F;
+    }
+    
+}
---
+++ hw/chicaiza/u3/hw19abstract/ec.edu.espe.client_AbstractFactory_jar_1.0-SNAPSHOT/src/main/java/ec/edu/espe/client/view/ClientApp.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espe.client.view;
+
+import ec.edu.espe.client.model.GUIFactory;
+import ec.edu.espe.client.model.Button;
+
+
+/**
+ *
+ * @author Chicaiza Michael
+ */
+public class ClientApp {
+
+    public static void main(String[] args) {
+        GUIFactory aFactory = GUIFactory.getFactory();
+        Button aButton = aFactory.createButton();
+        aButton.caption="Play";
+        aButton.paint();
+    }
+}
