925ad99db1078d493ba7b81676f2d9a546ac4594
Pedro Isaac Maisincho Paucar <imasincho@gmail.com>
2025-07-30 00:01:38 -0500

    AbstractF

---
+++ hw/maisincho/u3/HW19AbstractFactory/AbstractFactory/src/main/java/ec/edu/espe/abstractFactory/controller/WinFactory.java
@@ -0,0 +1,24 @@
+package ec.edu.espe.abstractFactory.controller;
+
+import ec.edu.espe.abstractFactory.model.Button;
+import ec.edu.espe.abstractFactory.model.Menu;
+import ec.edu.espe.abstractFactory.model.WinMenu;
+
+/**
+ *
+ * @author Isaac Maisincho Crafters_Market DCCO ESPE
+ */
+public class WinFactory extends GUIFactory {
+
+    @Override
+    public Button createButton() {
+        return (new WinButton());
+    }
+
+    @Override
+    public Menu createMenu() {
+        return (new WinMenu());
+
+    }
+
+}
