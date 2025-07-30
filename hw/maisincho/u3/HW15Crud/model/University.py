707128050de332f34356487ad6b92d69104b5439
Pedro Isaac Maisincho Paucar <imasincho@gmail.com>
2025-07-29 23:58:04 -0500

    CRUD

---
+++ hw/maisincho/u3/HW15Crud/model/University.py
@@ -0,0 +1,8 @@
+
+
+class University:
+    def __init__(self, id, name, monthlyIncome, numberOfStudent):
+        self.id = id
+        self.name = name
+        self.monthlyIncome = monthlyIncome
+        self.numberOfStudent = numberOfStudent
