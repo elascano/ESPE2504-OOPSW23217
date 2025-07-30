cfb0c688912dde68f8164618fabdb659e6cf918a
David Alejandro Bonilla Caiza <davidbonilla188@gmail.com>
2025-07-30 08:06:12 -0500

    HW18 update

---
+++ hw/bonilla/U3/HW18AbstractFactory/GUIFactoryCorrect/src/main/java/ec/edu/espe/guifactory/view/WinMenu.java
@@ -0,0 +1,15 @@
+package ec.edu.espe.guifactory.view;
+
+import ec.edu.espe.guifactory.model.Menu;
+
+
+/**
+ *
+ * @author Bonilla David SoftCrafters
+ */
+public class WinMenu extends Menu {
+
+    public void paint() {
+        System.out.println("I'm a WinMenu: " + caption);
+    }
+}
