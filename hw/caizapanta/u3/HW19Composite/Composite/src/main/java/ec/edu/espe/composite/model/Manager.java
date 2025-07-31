75dfb450d93684bb10442acc504b927d4f058125
tammycaizapanta <tammycaizapanta95@gmail.com>
2025-07-31 01:32:38 -0500

    HW19Composite V1.0

---
+++ hw/caizapanta/u3/HW19Composite/Composite/src/main/java/ec/edu/espe/composite/model/Manager.java
@@ -0,0 +1,24 @@
+package ec.edu.espe.composite.model;
+
+/**
+ *
+ * @author Tammy Caizapanta SoftCrafters DCCO ESPE
+ */
+public class Manager extends Supervisor {
+
+    public Manager(String aName) {
+        this();
+        name = aName;
+
+    }
+
+    public Manager() {
+        super();
+        title = "Manager";
+    }
+
+    public void stateName() {
+        //do processing special to manager naming
+        super.stateName();
+    }
+}
