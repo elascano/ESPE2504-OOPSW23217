7faac3fd2ae04aab0dc50323b0987e6356c93dc4
David Alejandro Bonilla Caiza <davidbonilla188@gmail.com>
2025-08-01 17:59:26 -0500

    HW update

---
+++ hw/bonilla/U3/HW15CRUD/CellphoneCRUD/src/main/java/ec/edu/espe/cellphonecrud/controller/CellphoneCRUD.java
@@ -0,0 +1,15 @@
+package ec.edu.espe.cellphonecrud.controller;
+
+import ec.edu.espe.cellphonecrud.view.Menu;
+
+/**
+ *
+ * @author Bonilla David Softcrafters
+ */
+public class CellphoneCRUD {
+
+    public static void main(String[] args) {
+        Menu.principalMenu();
+    }
+
+}
