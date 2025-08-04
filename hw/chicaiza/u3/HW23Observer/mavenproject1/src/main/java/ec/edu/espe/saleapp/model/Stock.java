c6ad9439db7dad732f3c6aa118a2c5fe172a76fe
Chicaiza Amaguaña Michael Alexis <michaelch3002@gmail.com>
2025-08-04 10:32:42 -0500

    hw23

---
+++ hw/chicaiza/u3/HW23Observer/mavenproject1/src/main/java/ec/edu/espe/saleapp/model/Stock.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.saleapp.model;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ *
+ * @author Chicaiza Michael
+ */
+abstract class Stock {
+    protected String symbol; 
+    protected double price; 
+    private ArrayList investors = new ArrayList();
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public Stock() { }
+    public void addObserver( IInvestor iinvestor ) {
+
+        investors.add( iinvestor );
+    } 
+    
+    public void deleteObserver( IInvestor iinvestor ) { 
+
+        investors.remove( iinvestor );
+    } 
+    public void notifyObservers(Object args) {
+        Iterator i = investors.iterator();
+        
+        while( i.hasNext() ) {
+            IInvestor investor = (IInvestor)i.next();
+            investor.update(this,args );
+
+        }
+    } 
+
+
+}
