7052a58615518368a3ea5cf2cdf4d0f92f92ee6f
Kevin Vaca <kevin.vaca.05@hotmail.com>
2025-08-05 10:54:49 -0500

    HwStrategy

---
+++ hw/vaca/u3/HWStrategy/src/main/java/ec/edu/espe/hwstrategy/SortingStrategy.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package ec.edu.espe.hwstrategy;
+
+/**
+ *
+ * @author Kevin Vaca Edison's OOP ESPE
+ */
+public interface SortingStrategy {
+        int[] sort(int[] data);
+}
