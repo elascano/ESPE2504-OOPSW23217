bac2bfa4c283d523d9ed9d3eee12bbd3929a2ee6
Jorge-Luis-Fuentes-Gualotuña <jorgeluisfuentes6ahmls@gmail.com>
2025-08-06 13:21:35 -0500

    Add files via upload

---
+++ ws/fuentes/U3/WS37ProjectStructure/ChickenFarm/src/main/java/ec/edu/espe/chickenfarmer/model/Chicken.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.chickenfarmer.model;
+
+import ec.edu.espe.chickenfarmer.controller.ChickenController;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author LABS-ESPE
+ */
+public class Chicken extends ChickenController {
+    int id;
+    String BornOnDate;
+    boolean isMolting;
+    String color;
+
+    @Override
+    public ArrayList<Chicken> readAll() {
+        //validations, buisnes rules
+        //using utils to read data from MongoDB atlas
+        return new ArrayList<Chicken>();
+    }
+
+    @Override
+    public void assignCoop(Chicken chicken, ChickenCoop coop) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+    
+}
