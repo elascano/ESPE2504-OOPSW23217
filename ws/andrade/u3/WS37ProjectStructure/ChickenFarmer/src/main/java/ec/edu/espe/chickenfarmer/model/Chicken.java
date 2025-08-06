c3745e5fcdd8afc5610b30b18fd1bc2d45f1783c
julitobby <julitobbyandrade@gmail.com>
2025-08-06 13:20:56 -0500

    Ws35

---
+++ ws/andrade/u3/WS37ProjectStructure/ChickenFarmer/src/main/java/ec/edu/espe/chickenfarmer/model/Chicken.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.chickenfarmer.model;
+
+/**
+ *
+ * @author LABS-ESPE
+ */
+public class Chicken {
+    int id;
+    String bornDate;
+    boolean isMolting;
+    String color;
+    
+}
